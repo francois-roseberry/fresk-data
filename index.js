6c226bc272830032985adb51f626540518e2f058
froseberry <froseberry@synack.com>
2018-03-15 17:19:01 -0400

    Add rent extraction

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ request('http://tourfresk.com/plans/', (error, response, html) => {
   }
 
   const $ = cheerio.load(html);
+  const appartments = [];
   $('.noUnites').each(function () {
     const node = $(this);
     const nodes = node.find('h5');
-    const name = nodes.eq(0).children().eq(0).text();
-    console.log('Appartment node found', name);
+    const isReserved = nodes.eq(0).text() == 'UNITÉ RÉSERVÉE';
+    if (!isReserved) {
+      const name = readName(nodes);
+      const rent = readRent(nodes);
+      appartments.push({ name, rent });
+    }
   });
+
+  console.log(appartments);
 });
+
+const readName = nodes => nodes.eq(0).children().eq(0).text();
+
+const readRent = nodes => {
+  const monthlyRentString = nodes.eq(3).children().eq(0).text();
+  rentString = monthlyRentString.split('\$')[0].trim().replace(/\s/g, '');
+  return parseInt(rentString);
+}
